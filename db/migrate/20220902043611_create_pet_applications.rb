251793faea8bd6addee34a1b7d7d2b2496dc6fa3
Stephen Fabian <fabianstephen@gmail.com>
2022-09-03 11:52:29 -0600

    deletes incorrect migration for pet_applications

--- db/migrate/20220902043611_create_pet_applications.rb
+++
@@ -1,8 +0,0 @@
-class CreatePetApplications < ActiveRecord::Migration[5.2]
-  def change
-    create_table :pet_applications do |t|
-      t.references :pet
-      t.references :application
-    end
-  end
-end
